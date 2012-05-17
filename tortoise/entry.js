6127a776643c571226b1117a76949c70c1e901bc
James Taylor <jostylr@gmail.com>
2012-05-16 22:24:04 -0400

    Turtle drafted. preparing to assemble

--- tortoise/entry.js
+++ tortoise/entry.js
@@ -1,11 +1,13 @@
 /*globals module, require, console, exports*/
 
-var parser = require('tort').parse;
-var et = require('evaltort');
-var raph = require('raph');
+run = (function () {
+  var parser = require('tort').parse;
+  var et = require('evaltort');
+  var Turtle = require('Turtle');
 
-module.exports = function (str) {
-  var par = parser(str);
-  //console.log(par);
-  return et(par, raph.turtle());
-}
+  return function (str, opt) {
+    var par = parser(str);
+    //console.log(par);
+    return et(par, new Turtle(opt));
+  }  
+}());
